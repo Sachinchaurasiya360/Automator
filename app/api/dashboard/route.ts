5f4d196387369f4c6e2fc12ba9de97d7794dfa2f
Sachin Chaurasiya <mrsachinchaurasiya@gmail.com>
2026-04-28 20:33:40 +0530

    fix build issues

--- app/api/dashboard/route.ts
+++ app/api/dashboard/route.ts
@@ -4,6 +4,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { NextRequest, NextResponse } from "next/server";
 
+
 export async function GET() {
   try {
     const session = await getServerSession(authOptions);
--- notes.md
+++ notes.md
@@ -1,1 +1,3 @@
 throttling and debouncing
+
+in next js the body comes into the raw stream that's why we need to convert those stream into the json 
